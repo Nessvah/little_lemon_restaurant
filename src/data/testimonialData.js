9929740b4922f0680d0927def5ae5bcc37cbfb9e
Nessvah <info@silviavanessa.dev>
2023-01-27 01:49:23 +0000

    add data from testimonials

---
+++ src/data/testimonialData.js
@@ -0,0 +1,24 @@
+const testimonials = [{
+    name: "Andre",
+    image: "src/...",
+    comment: "Something about little lemon",
+    ratings: "Five stars"
+}, {
+    name: "Samantha",
+    image: "src/...",
+    comment: "Something about little lemon",
+    ratings: "Five stars"
+}, {
+    name: "Philip",
+    image: "src/...",
+    comment: "Something about little lemon",
+    ratings: "Five stars"
+}, {
+    name: "Joshua",
+    image: "src/...",
+    comment: "Something about little lemon",
+    ratings: "Five stars"
+}
+]
+
+export default testimonials;
