0b77f3b74e1ba5403412ccc1ee89bc0bfbbac952
Nessvah <info@silviavanessa.dev>
2023-03-02 16:14:34 +0000

    change the hero img size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,61 +1,89 @@
-import { VStack, HStack, Heading, Text, Button, Image, Stack} from "@chakra-ui/react";
+import {
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Stack,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import FullScreen from "./FullScreen";
-import Img from '../images/restauranfood.jpg';
-
+import Img from "../images/restauranfood.jpg";
 
 const Hero = () => {
+  // create navigation through the button element
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/reservations`;
+    navigate(path);
+  };
+  return (
+    <header>
+      <FullScreen>
+        <HStack position={"relative"} w={"860px"}>
+          <VStack
+            w={"40%"}
+            m={"2rem auto 2rem 0"}
+            alignItems={"flex-start"}
+            gap={"2rem"}
+          >
+            <Stack direction={"column"}>
+              {/*Find a way to get this two headings closer to each other*/}
+              <Heading
+                as={"h1"}
+                fontWeight={"regular"}
+                color={"Yellow"}
+                fontSize={64}
+              >
+                Little Lemon
+              </Heading>
+              <Heading as={"h2"} fontWeight={"regular"} fontSize={40} mt={0}>
+                Chicago
+              </Heading>
+            </Stack>
 
+            <Text fontWeight={"extrabold"} fontSize={20} mt={32}>
+              We are a family owned Meditarrean restaurant, focused on
+              traditional recipes served with a modern Twist.
+            </Text>
 
-    // create navigation through the button element
-    let navigate = useNavigate();
-    const routeChange = () => {
-        let path = `/reservations`;
-        navigate(path);
-    }
-    return (
-        <header>
-            <FullScreen>
-                <HStack position={'relative'} w={'860px'}>
-                    <VStack w={'40%'} m={'2rem auto 2rem 0'} alignItems={'flex-start'} gap={'2rem'}>
-                        <Stack direction={'column'}>
-                            {/*Find a way to get this two headings closer to each other*/}
-                            <Heading as={'h1'} fontWeight={'regular'} color={'Yellow'} lineHeight={'-2px'}
-                                     fontSize={64}>Little Lemon</Heading>
-                            <Heading as={'h2'} fontWeight={'regular'} fontSize={40} mt={0}>Chicago</Heading>
-                        </Stack>
-
-                        <Text fontWeight={'extrabold'} fontSize={20} mt={32}>We are a family owned Meditarrean restaurant,
-                            focused on traditional recipes served with a
-                            modern Twist.</Text>
-
-                        {/* change color for focus outline */}
-                        <Button onClick={routeChange} bg={'Yellow'} fontSize={18} color={'Dark'} fontWeight={'regular'}
-                                _hover={{ bg: '#FEE361' }}
-                                _active={{
-                                    bg: '#DBB602',
-                                    transform: 'scale(0.98)',
-                                    borderColor: 'Green',
-                                }}
-                                _focus={{
-                                    boxShadow:
-                                        '0 0 1px 2px rgba(251, 218, 187, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-                                }}>Reserve a table</Button>
-                    </VStack>
-                        <Image
-                            h={'400px'}
-                            objectFit='cover'
-                            borderRadius={'16'}
-                            src={Img}
-                            alt='Chef at restaurant holding dish'
-                            position={'absolute'}
-                            top={'60px'}
-                            right={0}/>
-                </HStack>
-
-            </FullScreen>
-        </header>
-    )
+            {/* change color for focus outline */}
+            <Button
+              onClick={routeChange}
+              bg={"Yellow"}
+              fontSize={18}
+              color={"Dark"}
+              fontWeight={"regular"}
+              _hover={{ bg: "#FEE361" }}
+              _active={{
+                bg: "#DBB602",
+                transform: "scale(0.98)",
+                borderColor: "Green",
+              }}
+              _focus={{
+                boxShadow:
+                  "0 0 1px 2px rgba(251, 218, 187, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+              }}
+            >
+              Reserve a table
+            </Button>
+          </VStack>
+          <Image
+            h={"380px"}
+            w={"33%"}
+            objectFit="cover"
+            borderRadius={"16"}
+            src={Img}
+            alt="Chef at restaurant holding dish"
+            position={"absolute"}
+            top={"60px"}
+            right={0}
+          />
+        </HStack>
+      </FullScreen>
+    </header>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,52 @@
-import {Link} from "react-router-dom";
-import { HStack, UnorderedList, ListItem, Box, Image} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { HStack, UnorderedList, ListItem, Box, Image } from "@chakra-ui/react";
 import Logo from "../images/Logo.svg";
 
-
 const Navbar = () => {
-
-    return (
-        <header className={'header'}>
-            <HStack maxW={'860px'} margin={'0 auto'} justifyContent={'space-between'} alignItems={'center'} h={'100%'}>
-                <Box>
-                    <Image width={'150px'} objectFit={'cover'} src={Logo} alt={'Little Lemon logo'}></Image>
-                </Box>
-                <nav>
-                    <UnorderedList styleType={'none'}>
-                        <HStack gap={6} fontWeight={'bold'} color={"Dark"}>
-                            <ListItem><Link to={"/"}>Home</Link></ListItem>
-                            <ListItem><Link to={"/"}>About</Link></ListItem>
-                            <ListItem><Link to={"/"}>Menu</Link></ListItem>
-                            <ListItem><Link to={"/reservations"}>Reservations</Link></ListItem>
-                            <ListItem><Link to={"/"}>Order Online</Link></ListItem>
-                            <ListItem><Link to={"/"}>Login</Link></ListItem>
-                        </HStack>
-                    </UnorderedList>
-                </nav>
+  return (
+    <header className={"header"}>
+      <HStack
+        maxW={"860px"}
+        margin={"0 auto"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        h={"100%"}
+      >
+        <Box>
+          <Image
+            width={"150px"}
+            objectFit={"cover"}
+            src={Logo}
+            alt={"Little Lemon logo"}
+          ></Image>
+        </Box>
+        <nav>
+          <UnorderedList styleType={"none"}>
+            <HStack gap={6} fontWeight={"bold"} color={"Dark"}>
+              <ListItem>
+                <p> Home</p>
+              </ListItem>
+              <ListItem>
+                <p>About</p>
+              </ListItem>
+              <ListItem>
+                <p>Menu</p>
+              </ListItem>
+              <ListItem>
+                <Link to={"/reservations"}>Reservations</Link>
+              </ListItem>
+              <ListItem>
+                <p>Order Online</p>
+              </ListItem>
+              <ListItem>
+                <p>Login</p>
+              </ListItem>
             </HStack>
-
-        </header>
-
-    )
-}
+          </UnorderedList>
+        </nav>
+      </HStack>
+    </header>
+  );
+};
 
 export default Navbar;
